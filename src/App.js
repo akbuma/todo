ef57298f033ca6114af80921d6025f22899c3f67
Aaron Bumanglag <xnagni@gmail.com>
2020-02-11 17:59:18 -0800

    Implemented functional components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import TodoItem from './TodoItem'
+import todosData from './todosData'
+import './style.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const todo = todosData.map(item => <TodoItem key={item.id} item={item}/>)
+
+    return (
+        <div className="todo-list">
+            {todo}
+        </div>
+    );
 }
 
-export default App;
+export default App
